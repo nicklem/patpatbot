d3d06ac07df46d0dde4bf62a1b4af904c343f9da
nicklem <nicola.klemenc@codacy.com>
2024-05-22 18:00:39 +0200

    feat: Inputs

--- src/lib/init.ts
+++ src/lib/init.ts
@@ -1,15 +1,25 @@
 import {config} from 'dotenv';
 import {glob} from 'glob';
 import {join} from 'path';
+import {getInput} from "@actions/core";
+import github from "@actions/github";
 
 // Load environment variables
 config();
 
 const ENV_OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-const ENV_DOCS_GLOB = process.env.INPUT_DOCS_GLOB;
-const ENV_DOC_DESCRIPTIONS_PATH = process.env.INPUT_DOC_DESCRIPTIONS_PATH;
-const ENV_DOC_PATTERNS_PATH = process.env.INPUT_DOC_PATTERNS_PATH;
-const ENV_REPO_NAME = process.env.REPO_NAME;
+const ENV_REPO_NAME = github?.context.repo.repo || process.env.REPO_NAME;
+
+// TODO Default values don't work with this function. Should they?
+const ENV_DOCS_GLOB =
+    getInput('docs_glob')
+    || "docs/description/*.md";
+const ENV_DOC_DESCRIPTIONS_PATH =
+    getInput('doc_descriptions_path')
+    || "docs/description/description.json";
+const ENV_DOC_PATTERNS_PATH =
+    getInput('doc_patterns_path')
+    || "docs/patterns.json";
 
 const PATHS_PROMPTS = glob.sync(join(__dirname, "..", "prompts", "*.json")).sort();
 
